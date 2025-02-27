a14940051f43146b8d9dd7cf7f7ac5f67abd6df9
sonia <sdjeuda@gamil.com>
2025-02-27 21:14:00 +0100

    first commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
+import ch.qos.logback.core.net.server.Client;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +14,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+@PostConstruct
+List<Client> addClent(){
+		ArrayList<Client>clients = new ArrayList<>();
+		return clients;
+}
 }
